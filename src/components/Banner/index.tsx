1d542e7837ff14cab017d41d14d00c104b9292e1
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-06 09:18:39 -0300

    feat: banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,13 @@
+export default function Banner() {
+    return (
+        <div
+            className="
+        w-full h-64 flex flex-col items-center justify-center gap-4
+        bg-[url('/background.png')] bg-center bg-cover 
+        text-indigo-50/50 "
+        >
+            <h1 className="text-6xl">Darley John</h1>
+            <p className="text-xl">Loja 100% focada no prazer</p>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.tsx'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                background: "url('/background.png')",
+            },
+        },
+    },
+    plugins: [],
 }
