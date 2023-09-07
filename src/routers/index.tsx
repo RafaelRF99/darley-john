2833192be85a3a7665cacf1ceba4714a67e7149e
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-07 09:47:39 -0300

    feat: suporte

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from '../page/App'
 import Error from '../page/Error'
 import Sell from '../page/Sell'
+import Suporte from '../page/Suporte'
 
 export default function AppRouter() {
     return (
@@ -9,6 +10,7 @@ export default function AppRouter() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/vender" element={<Sell />} />
+                <Route path="/suporte" element={<Suporte />} />
                 <Route path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
