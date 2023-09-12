ae99995683696425875a19d969803f5f057d539e
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-12 17:14:23 -0300

    chore: select + sub

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,10 +6,10 @@ export default function Banner() {
             className=" relative
         w-full h-64 flex flex-col items-center justify-center gap-4
         bg-[url('/background.png')] bg-center bg-no-repeat bg-zinc-950 
-        text-indigo-50/50 "
+        text-indigo-50/50 select-none"
         >
             <h1 className="text-6xl">Darley John</h1>
-            <p className="text-xl">Loja 100% focada no prazer</p>
+            <p className="text-xl">Venda sua moto custom aqui!</p>
             <div className="absolute bottom-0 w-full">
                 <MenuBar />
             </div>
