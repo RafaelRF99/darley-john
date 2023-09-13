2a892d952659d65393f63f3bd2291e92aa03489c
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-09-12 23:26:16 -0300

    feat: send msg

--- src/page/Suporte.tsx
+++ src/page/Suporte.tsx
@@ -4,13 +4,27 @@ export default function Suporte() {
     return (
         <div className="py-5">
             <h1 className="text-center">Contate-nos</h1>
-            <form className="flex justify-center">
+            <form
+                className="flex justify-center"
+                action="https://formsubmit.co/rafaelfferreira99@hotmail.com"
+                method="POST"
+            >
                 <div className="w-full mx-10 sm:max-w-lg flex flex-col gap-3 py-6">
                     <div className="flex flex-col">
                         <label className="font-semibold">Nome</label>
                         <input
                             type="text"
+                            name="name"
                             placeholder="Digite seu nome..."
+                            className="px-2 py-1"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="font-semibold">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="exemplo@gmail.com"
                             className="px-2 py-1"
                         />
                     </div>
@@ -18,6 +32,7 @@ export default function Suporte() {
                         <label className="font-semibold">Descrição</label>
                         <textarea
                             style={{ resize: 'none' }}
+                            name="description"
                             className="shadow-md px-3 py-2 h-56"
                             placeholder="Digite aqui..."
                         />
